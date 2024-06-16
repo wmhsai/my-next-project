ad15476562176196f481a8fcd686fa84c8a77171
m.malmir <mahsa.malmir769@gmail.com>
2024-06-16 16:02:38 +0330

    add rounting project

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styles from "../styles/About.module.css"
+const About = () => {
+    return (
+        <div className={Styles.container}>
+            <p className={Styles.About}>
+                About
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/news/[newsId].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsId = () => {
+  return (
+    <div>NewsId</div>
+  )
+}
+
+export default NewsId
